30310dfc2bc1eb261feb602961c58f1ce1c763b3
Golam26 <sarwar.golam@bjitgroup.com>
2019-11-19 16:27:48 +0600

    Added a new method from hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,9 @@
 plublic class Hello {
     Public static void main(String []args){
         System.out.println("VS code is Awesome");
+        greeting("Golam Sarwar");
+    }
+    Public static void greeting(string name){
+        System.out.println("Welcome " + name); 
     }
 }
